HARPS: Flux calibration attempts MARCS: fixed borders of specific intensity parts Fixed old bug in mu calculation, mu has to be given in radians
Former-commit-id: d9966affd06391fac12101e35e62bfc973a2d51c

exo_spectrum.py
@@ -93,12 +93,12 @@ def plot(conf, par, wl, obs, tell, flux, sol_t, source='psg'):
     except FileNotFoundError:
         is_planet = False
 
-    plt.plot(wl, tell, label='Telluric')
-    plt.plot(wl, obs[0], label='Observation')
-    plt.plot(wl, flux, label='Flux')
+    plt.plot(wl, normalize1d(tell), label='Telluric')
+    plt.plot(wl, normalize1d(obs[0]), label='Observation')
+    plt.plot(wl, normalize1d(flux), label='Flux')
     if is_planet:
         plt.plot(wl, planet, label='Planet')
-    # sol_t = normalize1d(sol_t)  # TODO
+    sol_t = normalize1d(sol_t)  # TODO
     plt.plot(wl, sol_t, label='Solution')
 
     plt.title('%s\nLambda = %.3g, S/N = %s' %
@@ -171,6 +171,14 @@ def get_data(conf, star, planet, **kwargs):
         intensities = factors.apply(lambda s: s * flux)
         wl_si = wl_flux
 
+    #TODO
+    
+    plt.plot(flux, label='flux')
+    for i in range(len(imu)):
+        plt.plot(intensities[imu[i]], label='%.3f' % imu[i])
+    plt.legend(loc='best')
+    plt.show()
+    
     print('   - Tellurics')
     if tellurics in ['psg']:
         wl_tell, tell = psg.load_tellurics(conf)
@@ -181,10 +189,11 @@ def get_data(conf, star, planet, **kwargs):
         tell = np.ones_like(wl_tell)
 
     print('   - Observations')
+    #phase is in radians
     if observation in ['psg']:
         wl_obs, obs, phase = psg.load_observation(conf)
     elif observation in ['harps']:
-        wl_obs, obs, phase = harps.load_observation(conf, par)
+        wl_obs, obs, phase = harps.load_observations(conf, par)
     elif observation in ['syn', 'synthetic', 'fake']:
         wl_obs, obs, phase = synthetic.generate_spectrum(
             conf, par, wl_tell, tell, wl_flux, flux, intensities, source='psg')
@@ -202,6 +211,8 @@ def get_data(conf, star, planet, **kwargs):
     tell = np.interp(wl, wl_tell, tell)
 
     # TODO DEBUG
+    if observation in ['harps']:
+        obs = harps.flux_calibration(conf, par, wl, obs)
 
     # Adding noise to the observation
     #noise = np.random.random_sample(obs.shape) / conf['snr']
@@ -279,7 +290,7 @@ if __name__ == '__main__':
         star = None
         planet = None
         #lamb = 'auto'
-        lamb = 0.1
+        lamb = 10000
 
     # TODO size of the atmosphere in units of planetar radii (scales and shifts the solution)
     atm_factor = 0.1

harps.py
@@ -7,11 +7,16 @@ import glob
 import numpy as np
 import pandas as pd
 import astropy.io.fits as fits
+from scipy.signal import correlate
+from scipy.optimize import minimize
+from scipy.interpolate import interp1d
+from scipy.ndimage.filters import gaussian_filter1d as gaussbroad
 
 import jdcal
 from awlib.astro import air2vac, doppler_shift
 
 import marcs
+
 
 def load(conf, par, fname, apply_barycentric=False):
     """ load a single fits file in the HARPS directory """
@@ -23,17 +28,17 @@ def load(conf, par, fname, apply_barycentric=False):
     wave = data['WAVE'][0, :]
     flux = data['FLUX'][0, :]
     wave = air2vac(wave)
-    
-    tmid = header['TMID'] #in mjd
-    #dtmid = mjd2datetime(tmid) #do I actually need that?
 
-    #phase?
+    tmid = header['TMID']  # in mjd
+    # dtmid = mjd2datetime(tmid) #do I actually need that?
+
+    # phase?
     transit = par['transit'] - jdcal.MJD_0
     period = par['period']
-    phase = ((tmid - (transit - period/2)) / period) % 1
+    phase = ((tmid - (transit - period / 2)) / period) % 1
     phase = 360 * phase
 
-    #barycentric velocity
+    # barycentric velocity
     if apply_barycentric:
         bc_velocity = -hdulist[0].header['ESO DRS BERV']
         flux = doppler_shift(wave, flux, bc_velocity)
@@ -57,8 +62,10 @@ def load_observations(conf, par):
     wl = np.array(wl)
     obs = np.array(obs)
     phase = np.array(phase)
+    phase = np.deg2rad(phase)
 
     return wl[0], obs, phase
+
 
 def load_flux(conf, par):
     """
@@ -66,13 +73,15 @@ def load_flux(conf, par):
     Requires some observations out of transit
     """
     wl, flux, phase = load_observations(conf, par)
-    flux = flux[(phase > 181) | (phase < 179)] #Don't use observations during transit 
+    # Don't use observations during transit
+    flux = flux[(phase > 181) | (phase < 179)]
     total = np.mean(flux)
     avg = np.mean(flux, 1)
-    flux = flux * total/avg[:, None]
+    flux = flux * total / avg[:, None]
     wl = wl[0]
     flux = np.mean(flux, 0)
     return wl, flux
+
 
 def load_tellurics(conf, par):
     """
@@ -91,50 +100,67 @@ def load_tellurics(conf, par):
 
     return wl, tell
 
+
 def load_solar(conf, par, reference='Vesta.fits'):
     """ load the HARPS reflected solar spectrum """
-    fname = join(conf['input_dir'], conf['harps_dir'], conf['harps_calibration_dir'], reference)
+    fname = join(conf['input_dir'], conf['harps_dir'],
+                 conf['harps_calibration_dir'], reference)
     r_wave, r_flux, _ = load(conf, par, fname, apply_barycentric=True)
     return r_wave, r_flux
 
-def flux_calibration(conf, par, wl, obs):
-    calib_dir = join(conf['input_dir'], conf['harps_dir'], conf['harps_calibration_dir'])
 
-    #load harps observation of Vesta (or other object)
+def flux_calibration(conf, par, wl, obs):
+    calib_dir = join(conf['input_dir'], conf['harps_dir'],
+                     conf['harps_calibration_dir'])
+
+    # TODO automatically find best offset and broadening
+    # Use Cross Correlation?
+    # load harps observation of Vesta (or other object)
     reference = 'Vesta.fits'
     r_wave, r_flux = load_solar(conf, par, reference)
-    r_flux = doppler_shift(r_wave, r_flux, par['radial_velocity'])
-    r_flux = np.interp(wl, r_wave, r_flux)
-    
-    """
-    #load marcs solar spectrum
-    s_wave, s_flux = marcs.load_solar(conf, par, calib_dir)
-    s_flux = np.interp(wl, s_wave, s_flux)
-    """
-    # Load SAO2010 spectrum
-    fname = join(calib_dir, 'sao2010.txt')
-    df = pd.read_table(fname, delim_whitespace=True, comment='#')
-    #print(df.head())
-    s_wave = df['WAVE'].values * 10 #Angstrom
-    #TODO
-    s_flux = df['Flux'].values * 1e4 #erg/s/cm**2/Å
-    s_flux = doppler_shift(s_wave, s_flux, par['radial_velocity'])
-    s_flux = np.interp(wl, s_wave, s_flux)
+    #r_flux = doppler_shift(r_wave, r_flux, par['radial_velocity'])
+    r_flux = interp1d(r_wave, r_flux, kind='quadratic',
+                      bounds_error=False, fill_value=0)(wl)
+    #r_flux = gaussbroad(r_flux, sigma = 2)
 
+    # load marcs solar spectrum
+    s_wave, s_flux = marcs.load_solar(conf, par, calib_dir)
+    s_flux = interp1d(s_wave, s_flux, kind='quadratic')(wl)
+    
     t_wave, t_flux = load_tellurics(conf, par)
-    t_flux = np.interp(wl, t_wave, t_flux)
+    t_flux = interp1d(t_wave, t_flux, kind='quadratic', bounds_error=False)(wl)
+    #t_flux = gaussbroad(t_flux, 2)
 
     s_flux *= t_flux
+    s_flux = gaussbroad(s_flux, 4)
 
-    #TODO
+
+    def func(x):
+        #I would split this for readability if that wouldn't take too much time
+        return -np.correlate(s_flux, gaussbroad(doppler_shift(wl, r_flux, x[0], 'linear'), abs(x[1])))[0]
+
+    sol = minimize(func, x0=[31.45, 2])
+    v, b = sol.x
+    r_flux = doppler_shift(wl, r_flux, v)
+    r_flux = gaussbroad(r_flux, b)
+    #s_flux = gaussbroad(s_flux, b2)
+
+    # compare
+    profile = np.where(s_flux != 0, r_flux / s_flux, 0)
+    profile = gaussbroad(profile, 1000)
+    calibrated = np.where(profile[None, :] > 0.2, obs / profile[None, :], 0)
+
+    """
+    # TODO
     import matplotlib.pyplot as plt
-    plt.plot(wl, s_flux * 2, label='solar')
-    plt.plot(wl, r_flux / 3, label='reference')
-    plt.plot(wl, obs * 40, label='observation')
-    #plt.xlim([5890, 5900])
+    plt.plot(wl, s_flux, label='solar')
+    plt.plot(wl, r_flux, label='reference')
+    plt.plot(wl, obs * 100, label='observation')
+    plt.plot(wl, profile * 100000, label='profile')
+    #plt.plot(wl, calibrated[0] * 100, label='calibrated')
+    #plt.xlim([5168, 5168.4])
+    plt.ylim([0, 2e5])
     plt.legend(loc='best')
     plt.show()
-    
-    #compare
-    profile = r_flux / s_flux
-    return  obs/profile[None, :]
+    """
+    return calibrated

intermediary.py
@@ -58,7 +58,7 @@ def interpolate_intensity(mu, i):
     return interp1d(i.keys().values, i.values, kind='quadratic', fill_value=0, bounds_error=False, copy=False)(mu).swapaxes(0, 1)
 
 def calc_mu(par, phase):
-    """ calculate the distance from the center of the planet to the center of the star as seen from earth """
+    """ calculate the distance from the center of the planet to the center of the star as seen from earth (in radians) """
     """
     distance = self.par['sma'] / self.par['r_star'] * \
         np.sqrt(np.cos(self.par['inc'])**2 +
@@ -104,11 +104,11 @@ def calc_intensity(par, phase, intensity, min_radius, max_radius, n_radii, n_ang
     intens = np.average(intens, axis=2, weights=radii)
     return intens
 
-def maximum_phase(self):
+def maximum_phase(par):
     """ The maximum phase for which the planet is still completely inside the stellar disk """
     # This is the inverse of calc_mu(maximum_phase()) = 1.0
-    return np.arcsin(np.sqrt(((self.par['r_star'] - self.par['r_planet'] - self.par['h_atm']) / (
-        self.par['sma'] * np.sin(self.par['inc'])))**2 - np.tan(self.par['inc'])**-2))
+    return np.arcsin(np.sqrt(((par['r_star'] - par['r_planet'] - par['h_atm']) / (
+        par['sma'] * np.sin(par['inc'])))**2 - np.tan(par['inc'])**-2))
 
 def specific_intensities(par, phase, intensity, n_radii=11, n_angle=7, mode='precise'):
     """

marcs.py
@@ -14,7 +14,7 @@ from scipy.interpolate import interp1d
 from scipy.constants import c, pi
 
 from DataSources import Cache
-from awlib.astro import air2vac
+from awlib.astro import air2vac, doppler_shift
 
 ###
 #  STELLAR FLUX
@@ -53,9 +53,10 @@ def load_flux(config, par):
     return wl, flux
 
 
-def load_flux_directly(conf, par):
+def load_flux_directly(conf, par, fname=None, apply_air2vac=True):
     """ Load MARCS flux directly from flx file """
-    fname = join(conf['input_dir'], conf['marcs_dir'], conf['marcs_file_flux'])
+    if fname is None:
+        fname = join(conf['input_dir'], conf['marcs_dir'], conf['marcs_file_flux'])
     df = pd.read_table(fname, delim_whitespace=True, names=[
                        'wl', 'rel_flux', 'abs_flux', 'rel_flux_conv', 'abs_flux_conv'], skiprows=1)
 
@@ -67,12 +68,10 @@ def load_flux_directly(conf, par):
     if 'marcs_wl_mod' in conf.keys():
         wl *= conf['marcs_wl_mod']
 
-    wl = air2vac(wl)
+    if apply_air2vac:
+        wl = air2vac(wl)
 
-    v = par['radial_velocity']
-    c_loc = c * 1e-3
-    shift = (1 + v / c_loc) * wl
-    flux = np.interp(wl, shift, flux)
+    flux = doppler_shift(wl, flux, par['radial_velocity'])
 
     return wl, flux
 
@@ -161,9 +160,10 @@ def read_all(fname, imu, ld_format, interpolate='linear'):
     to: last value
     interpolate: interpolation method
     """
-    result = [read(fname.format(i), imu, interpolate=interpolate)
+    result = [read(fname.format(i), imu, interpolate=interpolate)[100:-100]
               for i in ld_format]
-    result = np.concatenate(result)
+    
+    result = np.concatenate(result)[:-200]
     return result
 
 
@@ -225,16 +225,5 @@ def load_limb_darkening(config, par):
 
 def load_solar(conf, par, calib_dir):
     s_fname = join(calib_dir, 'sun.flx')
-    df = pd.read_csv(s_fname, header=None, names=['FLUX'])
-    s_flux = df['FLUX'].values
-
-    s_wl_fname = join(calib_dir, 'flx_wavelengths.vac')
-    df = pd.read_csv(s_wl_fname, header=None, names=['WAVE'])
-    s_wave = df['WAVE'].values
-
-    if 'marcs_flux_mod' in conf.keys():
-        s_flux *= float(conf['marcs_flux_mod'])
-    if 'marcs_wl_mod' in conf.keys():
-        s_wave *= float(conf['marcs_wl_mod'])
-
-    return s_wave, s_flux
+    s_wave, s_flux = load_flux_directly(conf, par, s_fname, apply_air2vac=False)
+    return s_wave, s_flux / (2 * np.pi)

synthetic.py
@@ -13,9 +13,11 @@ import psg
 def generate_spectrum(conf, par, wl_tell, telluric, wl_flux, flux, intensity, source='psg'):
     """ Generate a fake spectrum """
 
-    phase_file = join(conf['input_dir'], conf['psg_dir'], conf['psg_file_phase'])
-    phase = pd.read_table(phase_file, delim_whitespace=True, index_col='filename')
-    phase = phase.values[:, 0]
+    #TODO determine suitable phases independently
+    max_phase = iy.maximum_phase(par)
+    n_obs = 20
+    phase = np.linspace(180-max_phase, 180 + max_phase, num=n_obs)
+    phase = np.deg2rad(phase)
 
     # Sigma of Instrumental FWHM in pixels
     sigma = 1 / 2.355 * conf['fwhm']

test.py
@@ -12,7 +12,7 @@ from awlib.util import interpolate_DataFrame
 from DataSources.PSG import PSG
 
 #from test_project.Plot import Plot
-
+import intermediary as iy
 import config
 import stellar_db
 import marcs
@@ -51,6 +51,12 @@ wl_m3, intensities = marcs.load_intensities(conf, par)
 """
 #Load HARPS
 wl_harps, flux_harps, phase = harps.load_observations(conf, par)
+bpmap = iy.create_bad_pixel_map(flux_harps, threshold=1e-3)
+bpmap[wl_harps > 6800] = True
+
+flux_harps = flux_harps[:, ~bpmap]
+wl_harps = wl_harps[~bpmap]
+
 #flux_harps[0] = doppler_shift(wl_harps, flux_harps[0], -par['radial_velocity'])
 
 #Average HARPS flux
@@ -60,12 +66,11 @@ flux_harps = flux_harps * total/avg[:, None]
 wl_harps = wl_harps
 flux_harps = np.mean(flux_harps, 0)
 
-
 flux_calib = harps.flux_calibration(conf, par, wl_harps, flux_harps)[0]
 
 plt.plot(wl_marcs, flux_marcs, label='marcs')
 plt.plot(wl_harps, flux_harps * 100, label='original')
-plt.plot(wl_harps, flux_calib * 1000, label='calibrated')
+plt.plot(wl_harps, flux_calib * 100, label='calibrated')
 plt.legend(loc='best')
 plt.show()
 