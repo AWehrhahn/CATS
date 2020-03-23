5c668ae70a2f53cb44893843fed1f3ffcad55fa9
Ansgar Wehrhahn <ansgar.wehrhahn@physics.uu.se>
2020-03-23 11:13:40 +0100

    add config.json to setup

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 from setuptools import setup, find_packages
+from shutil import copyfile
+from os.path import dirname, join, expanduser
 
 import versioneer
 
 # TODO Create and populate ~/.cats/config.json
+config_file = expanduser("~/.cats/config.json")
+local_file = join(dirname(__file__), "config.json")
+copyfile(local_file, config_file)
 
 setup(
     name="cats",
+    description="Characterization of exoplanet Atmospheres with Transit Spectroscopy",
     author="Ansgar Wehrhahn",
     author_email="ansgar.wehrhahn@physics.uu.se",
     packages=find_packages(),
