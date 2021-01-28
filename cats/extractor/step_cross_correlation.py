f38788b58081e77827a0ab209ea5af3f63b8801f
Ansgar Wehrhahn <31626864+AWehrhahn@users.noreply.github.com>
2021-01-28 12:26:04 +0100

    move radtrans to step

--- cats/extractor/step_cross_correlation.py
+++ cats/extractor/step_cross_correlation.py
@@ -1,8 +1,12 @@
 import numpy as np
-import radtrans as petit_radtrans
 from scipy.optimize import least_squares
+import matplotlib.pyplot as plt
 
 from flex.flex import FlexFile
+
+from astropy import units as u
+from petitRADTRANS import Radtrans
+from petitRADTRANS import nat_cst as nc
 
 from ..pysysrem.sysrem import sysrem
 from ..spectrum import Spectrum1D, Spectrum1DIO, SpectrumArray, SpectrumArrayIO
@@ -12,6 +16,85 @@ from .steps import Step, StepIO
 class PlanetAtmosphereReferencePetitRadtransStep(Step, Spectrum1DIO):
     filename = "reference_petitRADTRANS.npz"
 
+    def radtrans(self, wrange, star, planet):
+        # 0.8 to 5
+        wmin = wrange[0].to_value("um")
+        wmax = wrange[1].to_value("um")
+        # Initialize atmosphere
+        # including the elements in the atmosphere
+        atmosphere = Radtrans(
+            line_species=["H2O"],
+            # line_species=["H2O", "CO_all_iso", "CH4", "CO2", "Na", "K"],
+            rayleigh_species=["H2", "He"],
+            continuum_opacities=["H2-H2", "H2-He"],
+            wlen_bords_micron=[wmin, wmax],
+            mode="lbl",
+        )
+
+        # Define planet parameters
+        # Planet radius
+        R_pl = planet.radius.to_value("cm")
+        # R_pl = 1.838 * nc.r_jup_mean
+        # surface gravity
+        # gravity = 1e1 ** 2.45
+        gravity = planet.surface_gravity.to_value("cm/s**2")
+        # reference pressure (for the surface gravity and radius)
+        # TODO: ????
+        P0 = 0.01
+
+        # Pressure in bar
+        # has to be equispaced in log
+        print("Setup atmosphere pressures")
+        pressures = np.logspace(-6, 2, 100)
+        atmosphere.setup_opa_structure(pressures)
+
+        # Define temperature pressure profile
+        kappa_IR = 0.01  # opacity in the IR
+        gamma = 0.4  # ratio between the opacity in the optical and the IR
+        T_int = 200.0  # Internal temperature
+        # T_equ = 1500.0
+        T_equ = planet.teff_from_stellar(star.teff).to_value("K")
+        temperature = nc.guillot_global(
+            pressures, kappa_IR, gamma, gravity, T_int, T_equ
+        )
+
+        # Define mass fractions
+        mass_fractions = {}
+        mass_fractions["H2"] = 0.74 * np.ones_like(temperature)
+        mass_fractions["He"] = 0.24 * np.ones_like(temperature)
+        mass_fractions["H2O"] = 0.001 * np.ones_like(temperature)
+        # mass_fractions["CH4"] = 0.000001 * np.ones_like(temperature)
+        # mass_fractions["CO_all_iso"] = 0.01 * np.ones_like(temperature)
+        # mass_fractions["CO2"] = 0.00001 * np.ones_like(temperature)
+        # mass_fractions["Na"] = 0.00001 * np.ones_like(temperature)
+        # mass_fractions["K"] = 0.000001 * np.ones_like(temperature)
+
+        MMW = 2.33 * np.ones_like(temperature)
+
+        # Calculate transmission spectrum
+        print("Calculate transmission Spectrum")
+        atmosphere.calc_transm(
+            temperature, mass_fractions, gravity, MMW, R_pl=R_pl, P0_bar=P0
+        )
+        # atmosphere.calc_flux(temperature, mass_fractions, gravity, MMW)
+        wave = nc.c / atmosphere.freq / 1e-4
+        flux = 1 - (atmosphere.transm_rad / nc.r_sun) ** 2
+
+        wave = wave << u.um
+        flux = flux << u.one
+
+        spec = Spectrum1D(
+            spectral_axis=wave,
+            flux=flux,
+            description="petitRADTRANS transmission spectrum",
+            source="petitRADTRANS",
+            reference_frame="barycentric",
+            star=star,
+            planet=planet,
+        )
+
+        return spec
+
     def run(self, star, planet, detector):
         rv_padding = self.rv_padding
         wrange = detector.regions
@@ -20,7 +103,7 @@ class PlanetAtmosphereReferencePetitRadtransStep(Step, Spectrum1DIO):
         # Apply possible radial velocity tolerance of 200 km/s
         wmin *= 1 - rv_padding / 3e5
         wmax *= 1 + rv_padding / 3e5
-        ref = petit_radtrans.radtrans([wmin, wmax], star, planet)
+        ref = self.radtrans([wmin, wmax], star, planet)
         ref.star = star
         ref.planet = planet
 
@@ -132,6 +215,22 @@ class CrossCorrelationStep(Step, StepIO):
             filename = self.savefilename
         data = np.load(filename)
         return data
+
+    def plot(self, data, sysrem_iterations=2, sysrem_iterations_afterwards=4):
+        corr = data[f"{sysrem_iterations}.{sysrem_iterations_afterwards}"]
+
+        vmin, vmax = np.nanpercentile(corr, (1, 99))
+        plt.imshow(corr, origin="lower", aspect="auto", vmin=vmin, vmax=vmax)
+
+        plt.title(f"Cross-Correlation for SYSREM Iteration: {sysrem_iterations}")
+
+        xticks = plt.xticks()[0][1:-1]
+        xticks_labels = xticks - self.rv_range
+        plt.xticks(xticks, labels=xticks_labels)
+
+        plt.xlabel("rv [km/s]")
+        plt.ylabel("#Obs")
+        plt.show()
 
 
 class PlanetRadialVelocityStep(Step, StepIO):
